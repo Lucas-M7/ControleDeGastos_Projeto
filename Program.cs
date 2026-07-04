eae8a5379ee5d17549a3e61b916b388e7ec8ec4c
Lucas-M7 <Lucas015x@gmail.com>
2026-07-04 18:15:46 -0300

    feat: adiciona e configura o swagger para o teste de endpoints

--- Program.cs
+++ Program.cs
@@ -17,18 +17,31 @@ builder.Services.AddScoped<IRelatorioService, RelatorioService>();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+    {
+        Title = "Controle de Gastos - API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Controle de Gastos - API V1");
+    });
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
